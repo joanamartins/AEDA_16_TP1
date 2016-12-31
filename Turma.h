f81bddfcd360e5bc451b632c1ea78990c390d83b
gitinho <thug6757@gmail.com>
2016-12-31 17:17:23 +0000

    fixed class vacancies

--- Turma.h
+++ Turma.h
@@ -7,16 +7,16 @@ using namespace std;
 
 class Turma {
 	int id;
-	int vagasTotal;
+	int vagasMax;
 	vector<pair<string,int>> vagas;
 public:
-	Turma() { id = idg; vagasTotal = 0; idg++; };
+	Turma() { id = idg; vagasMax = 0; idg++; };
 	Turma(vector<pair<string, int>> vac) : vagas(vac) { id = idg; idg++; };
 	static int idg;
 	int getID() { return id; };
-	void addUC(string uc, int vac) { vagas.push_back(make_pair(uc, vac)); vagasTotal += vac; };
-	int getVac() { return vagasTotal; };
+	void addUC(string uc, int vac) { vagas.push_back(make_pair(uc, vac)); if (vac > getVac()) vagasMax = vac; };
+	int getVac() { return vagasMax; };
 	vector<pair<string, int>> getVagas() const { return vagas; };
-	void decVac() { for (size_t i = 0; i < vagas.size(); i++) { vagas[i].second--;	vagasTotal--; } };
-	bool operator<(Turma that) { return this->vagasTotal < that.vagasTotal; };
+	void decVac() { for (size_t i = 0; i < vagas.size(); i++) { vagas[i].second--;	vagasMax--; } };
+	bool operator<(Turma that) { return this->getVac() < that.getVac(); };
 };
